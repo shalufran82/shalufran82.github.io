6e779c7e3505a13b378cb47c803e6c7a3dbb0d9c
shalini francis <shalufran@gmail.com>
2021-10-18 16:29:08 +0800

    question 3

---
+++ js/haversine_main.js
@@ -0,0 +1,7 @@
+let ellocate = document.getElementById("locate")
+ellocate.addEventListener("click", function(){
+    if(navigator.geolocation){
+        let elLat = document.getElementById("lat")
+        let elLat = document.
+    })
+})
